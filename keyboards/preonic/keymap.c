08392b1db7b1173b3cace4f2174c5e4eb3bbc8c1
Namit Nathwani <namsnath@gmail.com>
2022-07-15 00:47:20 +0530

    update: backspace position on base layer
    - moves backspace to left space
    - replaces existing backspace with delete

--- keyboards/preonic/keymap.c
+++ keyboards/preonic/keymap.c
@@ -14,7 +14,7 @@ enum preonic_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base Layer
     *   ╔══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╗
-    *   ║   `  ~   │   1  !   │   2  @   │   3  #   │   4  $   │   5  %   │   6  ^   │   7  &   │   8  *   │   9  (   │   0  )   │   Bksp   ║
+    *   ║   `  ~   │   1  !   │   2  @   │   3  #   │   4  $   │   5  %   │   6  ^   │   7  &   │   8  *   │   9  (   │   0  )   │  Delete  ║
     *   ╟──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────╢
     *   ║   Tab    │   Q      │   W      │   E      │   R      │   T      │   Y      │   U      │   I      │   O      │   P      │   Enter  ║
     *   ╟──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────╢
@@ -22,15 +22,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *   ╟──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────╢
     *   ║   Shift  │   Z      │   X      │   C      │   V      │   B      │   N      │   M      │   ,  <   │   .  >   │   /  ?   │   Shift  ║
     *   ╟──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────┼──────────╢
-    *   ║   Ctrl   │   MEDIA  │   Alt    │   Super  │   LOWER  │  Space   │  Space   │   RAISE  │   Super  │   Alt    │   MEDIA  │   Ctrl   ║
+    *   ║   Ctrl   │   MEDIA  │   Alt    │   Super  │   LOWER  │   Bksp   │  Space   │   RAISE  │   Super  │   Alt    │   MEDIA  │   Ctrl   ║
     *   ╚══════════╧══════════╧══════════╧══════════╧══════════╧══════════╧══════════╧══════════╧══════════╧══════════╧══════════╧══════════╝
     */
     [L_BASE] = LAYOUT_ortho_5x12(
-        KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_BSPC,
+        KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_DEL,
         KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_ENT,
         KC_ESC,    KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
         KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,
-        KC_LCTL, MO(L_MEDIA), KC_LALT,   KC_LGUI, MO(L_LOWER), KC_SPC,    KC_SPC, MO(L_RAISE),  KC_LGUI,   KC_RALT, MO(L_MEDIA), KC_RCTL
+        KC_LCTL, MO(L_MEDIA), KC_LALT,   KC_LGUI, MO(L_LOWER), KC_BSPC,   KC_SPC, MO(L_RAISE),  KC_LGUI,   KC_RALT, MO(L_MEDIA), KC_RCTL
     ),
     /* Lower Layer
     *   ╔══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╤══════════╗
